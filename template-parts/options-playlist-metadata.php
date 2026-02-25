fdbe65fc331f95ef7660e2b1158d8baaf7abdc93
Martin Cipriano <martincipriano@gmail.com>
2026-02-25 15:01:55 +0800

    Add playlist-specific metadata field options including thumbnail for the specific metadata select

---
+++ template-parts/options-playlist-metadata.php
@@ -0,0 +1,5 @@
+<option disabled selected value="">&mdash; Select field &mdash;</option>
+<option value="playlist_title">Title</option>
+<option value="playlist_description">Description</option>
+<option value="playlist_thumbnail">Thumbnail</option>
+<option value="playlist_video_count">Video Count</option>
