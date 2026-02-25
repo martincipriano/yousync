15a1f45edb5e9e9dfd2fa33b9f179beeaefabb58
Martin Cipriano <martincipriano@gmail.com>
2026-02-25 15:01:31 +0800

    Remove thumbnail from playlist condition fields since image fields cannot be used as conditions

--- template-parts/options-playlist-fields.php
+++ template-parts/options-playlist-fields.php
@@ -1,5 +1,4 @@
 <option disabled selected value="">&mdash; Select field &mdash;</option>
 <option value="playlist_title" data-type="text">Title</option>
 <option value="playlist_description" data-type="text">Description</option>
-<option value="playlist_thumbnail" data-type="text">Thumbnail</option>
 <option value="playlist_video_count" data-type="number">Video Count</option>
