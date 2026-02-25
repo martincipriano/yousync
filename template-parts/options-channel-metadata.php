f30ba227e16cba43c6b6739d369d6456ccbfb107
Martin Cipriano <martincipriano@gmail.com>
2026-02-25 15:02:11 +0800

    Add channel-specific metadata field options including thumbnail for the specific metadata select

---
+++ template-parts/options-channel-metadata.php
@@ -0,0 +1,7 @@
+<option disabled selected value="">&mdash; Select field &mdash;</option>
+<option value="channel_title">Channel Title</option>
+<option value="channel_description">Channel Description</option>
+<option value="profile_picture">Profile Picture</option>
+<option value="banner_image">Banner Image</option>
+<option value="subscriber_count">Subscriber Count</option>
+<option value="video_count">Video Count</option>
